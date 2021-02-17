e8f7016a87b98c236dd5ba492ab65768130eeb80
LucasDarkSasuke <70898565+LucasDarkSasuke@users.noreply.github.com>
2021-02-17 18:51:17 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentModule } from './student/student.module';
+import { NotFoundComponent } from './shared/component/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./student/student.module').then(m => m.StudentModule)
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
